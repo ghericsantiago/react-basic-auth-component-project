5559d33e2d6c3e3a3736261fe5c47e885448262e
Gheric Santiago <gheric.santiago@directwithhotels.com>
2019-08-02 10:05:59 +0800

    Follow up files

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "gvsantiago.auth0.com",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "jVan8XvjA549kiHA4OGLzm5jFcpvm910",
+    callbackUrl: "http://localhost:3000/callback"
+  };
